8e7e81365808d6e394e404e4778741bae2e22d20
VictorCancino <juanvictorcancino@gmail.com>
2024-10-17 18:48:05 -0600

    Se agregó la operación de actualización de datos

--- To_Do_API/main.py
+++ To_Do_API/main.py
@@ -40,3 +40,13 @@ def traer_tarea(tarea_id:int):
         if tarea.id == tarea_id:
             return tarea
     raise HTTPException(status_code=404,detail="Tarea no encontrada")
+
+
+#Actualizar tarea
+@app.put("/tareas/{tarea_id}", response_model=Tarea)
+def update_tarea(tarea_id: int, update_tarea: Tarea):
+    for index, tarea in enumerate(tareas):
+        if tarea.id == tarea_id:
+            tareas[index] = update_tarea
+            return update_tarea
+    raise HTTPException(status_code=404, detail="Tarea no encontrada")
