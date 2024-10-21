5d38abfe7798fd3298592724e2e68b714fba07e6
VictorCancino <juanvictorcancino@gmail.com>
2024-10-20 19:40:57 -0600

    Se creó el modelo de datos del proyecto

--- API_libros/main.py
+++ API_libros/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def traer_ruta():
     return {"message": "Bienvenido a nuestra API de gestión de libros"}
+
+#Modelo de datos 
+class libro(BaseModel):
+    id:int
+    title: str
+    author: str
+    description: str = None
+    year: int
+    available: bool = True 
+
+libros = []
