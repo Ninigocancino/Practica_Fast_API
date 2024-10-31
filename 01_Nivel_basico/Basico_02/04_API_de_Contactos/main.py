02f65be4e55dfed6bf19f01761fac11499930027
VictorCancino <juanvictorcancino@gmail.com>
2024-10-31 10:52:28 -0600

    Se importarón las librerias necesarias para poder crear el modelo de datos 'Canotactos' y se modificarón los endpoints 'traer_contactos' y 'agregar_contactos' para permitirles acceder al modelo de datos 'Contactos'

--- 01_Nivel_basico/Basico_02/04_API_de_Contactos/main.py
+++ 01_Nivel_basico/Basico_02/04_API_de_Contactos/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+
+#Esta lista guarda los contactos
+contactos = []
+
+#Esta es la estructura de datos para los contactos
+class Contacto(BaseModel):
+    nombre: str
+    email: str
+    telefono: str
+
 
 # Esta ruta verifica que la API este funcionando
 @app.get("/")
@@ -9,14 +21,15 @@ def leer_ruta():
 
 
 # Esta ruta muestra todos los contactos
-@app.get("/canotactos/")
+@app.get("/contactos/", response_model=List[Contacto])
 def traer_contactos():
-    return {"Mensaje": "Aquí se mostrará la lista de contactos"}
+    return  contactos
 
 # Esta ruta permite agregar un nuevo contacto
-@app.post("/Agregar/")
-def agregar_contacto():
-    return {"Mensaje" : "Aquí puedes agregar un contacto"}
+@app.post("/Agregar/", response_model=Contacto)
+def agregar_contacto(contacto: Contacto):
+    contactos.append(contacto)
+    return contacto
 
 
 
